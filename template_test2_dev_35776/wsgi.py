5bd58fb6836f830458240f51c355d2b6a021f9bf
Crowdbotics <engineering@crowdbotics.com>
2022-09-27 15:09:07 +0000

    Initial commit.

---
+++ template_test2_dev_35776/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for template_test2_dev_35776 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'template_test2_dev_35776.settings')
+
+application = get_wsgi_application()
